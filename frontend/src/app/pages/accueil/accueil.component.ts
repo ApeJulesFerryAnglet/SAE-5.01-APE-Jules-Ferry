54ea12e578a4dd826cd22d88ef59c790c120d8e1
EtienneDumai <etienne.dumai@gmail.com>
2025-12-15 13:26:43 +0100

    modification du composant de la page d'accueil (ts html) css très basique juste pour une representation basique

--- frontend/src/app/pages/accueil/accueil.component.ts
+++ frontend/src/app/pages/accueil/accueil.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from "../../components/footer/footer.component";
-
+import { ActualiteService } from '../../services/Actualite/actualite.service';
+import { EvennementService } from '../../services/Evennement/evennement.service';
+import { Evennement } from '../../models/Evennement/evennement';
+import { Actualite } from '../../models/Actualite/actualite';
 @Component({
   selector: 'app-accueil',
   standalone: true,
@@ -10,5 +13,16 @@ import { FooterComponent } from "../../components/footer/footer.component";
   styleUrl: './accueil.component.css'
 })
 export class AccueilComponent {
-
+  private listeActualites: Actualite[] = [];
+  private listeEvennements: Evennement[] = [];
+  private readonly actualiteService = inject(ActualiteService);
+  private readonly evennementService = inject(EvennementService);
+  ngOnInit() {
+    this.actualiteService.getAllActualites().subscribe((data) => {
+      this.listeActualites = data;
+    });
+    this.evennementService.getAllEvennements().subscribe((data) => {
+      this.listeEvennements = data;
+    });
+  }
 }
