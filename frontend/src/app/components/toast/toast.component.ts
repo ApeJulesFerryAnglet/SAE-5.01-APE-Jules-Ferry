9af2b42702ef88b70ebca67ee90049930d78286c
EtienneDumai <etienne.dumai@gmail.com>
2025-12-27 22:35:53 +0100

    ajout de la modale, fonctionne style à perfect

--- frontend/src/app/components/toast/toast.component.ts
+++ frontend/src/app/components/toast/toast.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TypeErreurToast } from '../../enums/TypeErreurToast/type-erreur-toast';
+import {ToastService} from '../../services/Toast/toast.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-toast',
@@ -8,5 +11,9 @@ import { Component } from '@angular/core';
   styleUrl: './toast.component.css'
 })
 export class ToastComponent {
-
+  private readonly toastService = inject(ToastService);
+  toast = toSignal(this.toastService.toast, { initialValue: null });
+  close(): void {
+    this.toastService.clear();
+  }
 }
