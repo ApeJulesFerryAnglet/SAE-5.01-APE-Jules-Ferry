e2fa16a0f7f31178a90d4bcc96abb081cdf0f1de
EtienneDumai <etienne.dumai@gmail.com>
2026-01-06 09:21:15 +0100

    ajout de la page du compte

---
+++ frontend/src/app/pages/compte-utilisateur/compte-utilisateur.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compte-utilisateur',
+  standalone: true,
+  imports: [],
+  templateUrl: './compte-utilisateur.component.html',
+  styleUrl: './compte-utilisateur.component.css'
+})
+export class CompteUtilisateurComponent {
+
+}
