686b8cea0b684cf0f0314f77a896269f496ab183
Etienne Dumai <etienne.dumai@gmail.com>
2026-01-08 22:22:00 +0100

    ajout d'un composant pour le formulaire

---
+++ frontend/src/app/components/forms/form-modifier-password/form-modifier-password.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-modifier-password',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-modifier-password.component.html',
+  styleUrl: './form-modifier-password.component.css'
+})
+export class FormModifierPasswordComponent {
+
+}
