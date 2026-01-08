07dc378ff7e91319af42949c18d6f041e7bf6232
Etienne Dumai <etienne.dumai@gmail.com>
2026-01-08 23:28:45 +0100

    composant form fonctionnel, css du bouton disbaled OK

--- frontend/src/app/pages/compte-utilisateur/compte-utilisateur.component.ts
+++ frontend/src/app/pages/compte-utilisateur/compte-utilisateur.component.ts
@@ -2,12 +2,13 @@ import { Component, inject } from '@angular/core';
 import { Utilisateur } from '../../models/Utilisateur/utilisateur';
 import { AuthService } from '../../services/Auth/auth.service';
 import { UtilisateurService } from '../../services/Utilisateur/utilisateur.service';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractControl, ValidationErrors } from '@angular/forms';
+import { FormModifierPasswordComponent } from "../../components/forms/form-modifier-password/form-modifier-password.component";
+
 
 @Component({
   selector: 'app-compte-utilisateur',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [FormModifierPasswordComponent],
   templateUrl: './compte-utilisateur.component.html',
   styleUrl: './compte-utilisateur.component.css'
 })
@@ -17,9 +18,11 @@ export class CompteUtilisateurComponent {
   loadingUser: boolean = true;
   utilisateurMdp!: Utilisateur;
   erreurLoadingUser: boolean = false;
+  
+  
+  // État UI
   modifierMdp: boolean = false;
-  modificationMdpForm!: FormGroup;
-  private readonly fb = inject(FormBuilder);
+  resetKey = 0;
   private readonly utilisateurService = inject(UtilisateurService);
   private readonly authService = inject(AuthService);
   ngOnInit(): void {
@@ -35,36 +38,7 @@ export class CompteUtilisateurComponent {
         console.error('Erreur lors de la récupération de l\'utilisateur courant', error);
       }
     });
-    this.modificationMdpForm = this.fb.group({
-      mot_de_passe_actuel: ['', [Validators.required]],
-      nouveau_mot_de_passe: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/)]],
-      confirmation_nouveau_mot_de_passe: ['', [Validators.required]]
-    }, {
-      validators: this.passwordMatchValidator
-    });
-  }
-  private passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
-    const oldPassword = control.get('mot_de_passe_actuel');
-    const password = control.get('nouveau_mot_de_passe');
-    const confirmPassword = control.get('confirmation_nouveau_mot_de_passe');
-    if (!password || !confirmPassword || !oldPassword) {
-      return null;
-    }
-    // Nettoyage de l'erreur si tout est OK
-    if (password.value === confirmPassword.value) {
-      if (confirmPassword.hasError('passwordMismatch')) {
-        confirmPassword.setErrors(null);
-      }
-      return null;
-    }
-    if (oldPassword.value === password.value) {
-      // Pose l'erreur sur le formulaire global
-      password.setErrors({ passwordMismatchOldNew: true });
-      return { passwordMismatchOldNew: true };
-    }
-    // Pose l'erreur UNIQUEMENT sur le champ confirmation
-    confirmPassword.setErrors({ passwordMismatch: true });
-    return { passwordMismatch: true };
+    
   }
   public logout(): void {
     this.authService.logout().subscribe({
@@ -78,30 +52,13 @@ export class CompteUtilisateurComponent {
   }
   public modifierMotDePasse(): void {
     this.modifierMdp = true;
+    this.resetKey++;
   }
-  public onSubmit(): void {
-    console.log('Formulaire soumis');
+  onMdpSubmitted(payload: { motDePasse: string }): void {
+    //Faire la transimission au service => backend
+  }
+  onMdpCancelled(): void {
     this.modifierMdp = false;
-    this.modificationMdpForm.reset();
-  }
-  public annulerModification(): void {
-    this.modifierMdp = false;
-    this.modificationMdpForm.reset();
-  }
-  // Getters pour accéder aux contrôles du formulaire dans le template
-  get mot_de_passe_actuel() {
-    return this.modificationMdpForm.get('mot_de_passe_actuel');
-  }
-  get nouveau_mot_de_passe() {
-    return this.modificationMdpForm.get('nouveau_mot_de_passe');
-  }
-  get confirmation_nouveau_mot_de_passe() {
-    return this.modificationMdpForm.get('confirmation_nouveau_mot_de_passe');
-  }
-  get motDePasseActuelInvalid(): boolean {
-    return this.modificationMdpForm.hasError('passwordMismatchOldNew');
-  }
-  get passwordMismatch(): boolean {
-    return this.modificationMdpForm.hasError('passwordMismatch');
+    this.resetKey++;
   }
 }
