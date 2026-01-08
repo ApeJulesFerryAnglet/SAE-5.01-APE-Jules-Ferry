fad2e3103cd0473ee5465bfea90135c0fb6d6400
EtienneDumai <etienne.dumai@gmail.com>
2026-01-08 08:45:49 +0100

    ajout guard, suppr de code inutile

--- frontend/src/app/pages/compte-utilisateur/compte-utilisateur.component.ts
+++ frontend/src/app/pages/compte-utilisateur/compte-utilisateur.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { Utilisateur } from '../../models/Utilisateur/utilisateur';
 import { AuthService } from '../../services/Auth/auth.service';
-import { UtilisateurService } from '../../services/Utilisateur/utilisateur.service';
 
 @Component({
   selector: 'app-compte-utilisateur',
@@ -13,29 +12,19 @@ import { UtilisateurService } from '../../services/Utilisateur/utilisateur.servi
 export class CompteUtilisateurComponent {
   currentUser: Utilisateur | null = null;
   isAuthenticated: boolean = false;
-  utilisateurInfo!: Utilisateur;
   loadingUser: boolean = true;
   erreurLoadingUser: boolean = false;
-  private readonly utilisateurService = inject(UtilisateurService);
   private readonly authService = inject(AuthService);
   ngOnInit(): void {
     this.authService.currentUser$.subscribe({
       next: (user) => {
         this.currentUser = user;
         this.isAuthenticated = user !== null;
-        this.utilisateurService.getUtilisateurById(user!.id_utilisateur).subscribe({
-          next: (utilisateur) => {
-            this.utilisateurInfo = utilisateur;
-            this.loadingUser = false;
-          },
-          error: (error) => {
-            console.error('Erreur lors de la récupération des détails de l\'utilisateur', error);
-            this.erreurLoadingUser = true;
-            this.loadingUser = false;
-          }
-        });
+        this.loadingUser = false;
       },
       error: (error) => {
+        this.erreurLoadingUser = true;
+        this.loadingUser = false;
         console.error('Erreur lors de la récupération de l\'utilisateur courant', error);
       }
     });
