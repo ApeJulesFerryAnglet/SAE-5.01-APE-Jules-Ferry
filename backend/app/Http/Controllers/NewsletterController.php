f00708dc3dbdd823443f8ec918b135208f0fdfb0
Theo Lague <tlague@snapp.fr>
2026-01-08 17:35:06 +0100

    Ajout de la vérification du mail enregistré

--- backend/app/Http/Controllers/NewsletterController.php
+++ backend/app/Http/Controllers/NewsletterController.php
@@ -18,7 +18,11 @@ class NewsletterController extends Controller
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'email' => 'required|email'
+            'email' => 'required|email|unique:abonnes_newsletter,email'
+        ], [
+            'email.required' => 'L\'adresse email est obligatoire.',
+            'email.email'    => 'Le format de l\'email n\'est pas valide.',
+            'email.unique'   => 'Cet email est déjà inscrit à notre newsletter !',
         ]);
 
         if ($validator->fails()) {
