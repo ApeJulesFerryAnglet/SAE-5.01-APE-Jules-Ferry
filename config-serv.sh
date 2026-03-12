fb92f8431e9e296fe16dd4c93dd2243df5092c09
EtienneDumai <etienne.dumai@gmail.com>
2026-03-12 10:08:44 +0100

    suppresion des sudo dans le script de config serveur

--- config-serv.sh
+++ config-serv.sh
@@ -7,35 +7,35 @@ echo "Ajout cronjob"
 apt install cron
 export EDITOR=nano
 echo "Suppression des anciennes versions éventuelles..."
-sudo apt remove -y docker docker-engine docker.io containerd runc || true
+apt remove -y docker docker-engine docker.io containerd runc || true
 
 echo "Installation des dépendances..."
-sudo apt update
-sudo apt install -y ca-certificates curl gnupg
+apt update
+apt install -y ca-certificates curl gnupg
 
 echo "Ajout de la clé GPG Docker..."
-sudo install -m 0755 -d /etc/apt/keyrings
+install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | \
-sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo "Ajout du dépôt Docker..."
 echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
 https://download.docker.com/linux/debian \
 $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo "Installation de Docker..."
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "Activation du service..."
-sudo systemctl enable docker
-sudo systemctl start docker
+systemctl enable docker
+systemctl start docker
 
 echo "Ajout de l'utilisateur au groupe docker..."
-sudo usermod -aG docker $USER
+usermod -aG docker $USER
 
 echo "Ajout de cert-bot"
 apt update
