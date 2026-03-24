6dd41bd5e67c7f24b9e6fd23d6502f778b56e8da
mouhakeita23@gmail.com <mouhakeita23@gmail.com>
2026-03-24 12:20:26 +0100

    Revert "Correction lint"
    This reverts commit b20381afdaf2817c4ef137d0d384223a7bc5a7c3.

--- frontend/src/app/pages/actualite-page/actualite-page.component.ts
+++ frontend/src/app/pages/actualite-page/actualite-page.component.ts
@@ -41,7 +41,7 @@ export class ActualitePageComponent implements OnInit {
   get filteredActualites(): Actualite[] {
     if (!this.listeActualites) return [];
 
-    const filtered = this.listeActualites.filter(actu => {
+    let filtered = this.listeActualites.filter(actu => {
       const qs = this.searchQuery.toLowerCase();
       return (actu.titre && actu.titre.toLowerCase().includes(qs)) || 
              (actu.contenu && actu.contenu.toLowerCase().includes(qs));
